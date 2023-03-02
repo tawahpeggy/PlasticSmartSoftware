7daf2ac8d0cc5b7757ca8d5ca8f88137965058e5
fionaanye <fiona.anye@go-groups.net>
2023-03-02 15:59:51 +0200

    created the folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    BlogComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
